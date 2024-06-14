0b83c788e440591fece6753a9907ac7b1d7281e7
Matheuz233 <matheusaugus233@gmail.com>
2024-06-14 12:35:18 -0300

    feat: 🔨 Configuração da Lógica de Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,28 @@
-import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 
 import mainImage from "../../assets/banner.svg";
-
 import styles from "./Login.module.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setError("");
 
     if (email === "usuario@exemplo.com" && password === "12345678") {
-      // Adicionar Redirecionamento
+      const userData = { email: email, password: password };
+      console.log(userData);
+      login(userData);
+      navigate("/home");
     } else {
       setError("Email ou Senha Inválida");
     }
@@ -25,7 +31,7 @@ const Login = () => {
   return (
     <div className={styles.login}>
       <img className={styles.img} src={mainImage} alt="Banner" />
-      
+
       <form className={styles.form} onSubmit={handleSubmit}>
         <h2>Bem-Vindo(a) ao ProfileLister!</h2>
         {error && <p className={styles.error}>{error}</p>}
