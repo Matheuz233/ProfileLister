600bc5514f1749073a6cabe047e11f8a50bb7263
Matheuz233 <matheusaugus233@gmail.com>
2024-06-15 10:43:46 -0300

    style: 💄 Estilização Visual do Card de Usuário

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,6 +1,8 @@
+import styles from "./UserCard.module.css"
+
 const UserCard = ({ user }) => {
   return (
-    <article>
+    <article className={styles.user_card}>
       <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
       <h2>{`${user.first_name} ${user.last_name}`}</h2>
       <p>Email: {user.email}</p>
