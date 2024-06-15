5203183a9aefebc4d50b60f06b595cf1bfc0c109
Matheuz233 <matheusaugus233@gmail.com>
2024-06-15 09:28:58 -0300

    feat: 🔨 Criação da Página Principal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,94 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useQuery } from "react-query";
+import { useState } from "react";
+import UserCard from "../../components/UserCard/UserCard";
+import { getUsersPage } from "../../api/axios";
+import Filter from "../../components/Filter/Filter";
 
-export default Home
+const Home = () => {
+  const [page, setPage] = useState(1);
+  const [appliedFilters, setAppliedFilters] = useState({
+    name: "",
+    email: "",
+    lastName: "",
+  });
+
+  const {
+    isLoading,
+    isError: queryError,
+    error,
+    data: users,
+    isFetching,
+    isPreviousData,
+  } = useQuery(["users", page], () => getUsersPage(page), {
+    keepPreviousData: true,
+  });
+
+  if (isLoading) return <p>Loading Users...</p>;
+
+  if (queryError) return <p>Error: {error.message}</p>;
+
+  // Aplicar filtros aos usuários
+  const filteredUsers = users.data.filter((user) => {
+    const nameMatch = user.first_name
+      .toLowerCase()
+      .includes(appliedFilters.name.toLowerCase());
+    const emailMatch = user.email
+      .toLowerCase()
+      .includes(appliedFilters.email.toLowerCase());
+    const lastNameMatch = user.last_name
+      .toLowerCase()
+      .includes(appliedFilters.lastName.toLowerCase());
+
+    return nameMatch && emailMatch && lastNameMatch;
+  });
+
+  const handleApplyFilters = (filters) => {
+    setAppliedFilters(filters);
+    setPage(1);
+  };
+
+  const firstPage = () => setPage(1);
+  const lastPage = () => setPage(users.total_pages);
+
+  const pagesArray = Array.from(
+    { length: users.total_pages },
+    (_, index) => index + 1
+  );
+
+  return (
+    <div>
+      <Filter onApplyFilters={handleApplyFilters} />
+
+      <nav className="nav-ex2">
+        <button onClick={firstPage} disabled={isPreviousData || page === 1}>
+          &lt;&lt;
+        </button>
+
+        {pagesArray.map((pg) => (
+          <button
+            key={pg}
+            onClick={() => setPage(pg)}
+            disabled={isPreviousData}
+          >
+            {pg}
+          </button>
+        ))}
+
+        <button
+          onClick={lastPage}
+          disabled={isPreviousData || page === users.total_pages}
+        >
+          &gt;&gt;
+        </button>
+      </nav>
+
+      {isFetching && <span className="loading">Loading...</span>}
+
+      {filteredUsers.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
