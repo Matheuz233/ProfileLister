d02f02c72efd31d466efef641858393e9f797a32
Matheus Augusto <matheusaugus233@gmail.com>
2024-06-16 14:53:53 -0300

    refactor: :recycle: Refatoração no Componente Filtro e Mundaça da Lógica de Pesquisa

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-
-import styles from "./Filter.module.css"
+import { useState, useEffect } from "react";
+import styles from "./Filter.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Filter = ({ onApplyFilters }) => {
   const [filters, setFilters] = useState({
@@ -21,49 +22,59 @@ const Filter = ({ onApplyFilters }) => {
     onApplyFilters(filters);
   };
 
-  const clearFilters = () => {
-    setFilters({
-      name: "",
-      email: "",
-      lastName: "",
-    });
-    onApplyFilters({
-      name: "",
-      email: "",
-      lastName: "",
-    });
-  };
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (
+        e.key === "Enter" &&
+        ["name", "email", "lastName"].includes(document.activeElement.name)
+      ) {
+        handleApplyFilters();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [filters, handleApplyFilters]);
 
   return (
-    <div className={styles.filter}>
-      <input
-        className={styles.input}
-        type="text"
-        name="name"
-        placeholder="Filtrar por Nome"
-        value={filters.name}
-        onChange={handleFilterChange}
-      />
-      <input
-        className={styles.input}
-        type="text"
-        name="email"
-        placeholder="Filtrar por Email"
-        value={filters.email}
-        onChange={handleFilterChange}
-      />
-      <input
-        className={styles.input}
-        type="text"
-        name="lastName"
-        placeholder="Filtrar por Sobrenome"
-        value={filters.lastName}
-        onChange={handleFilterChange}
-      />
+    <div>
+      <h1 className={styles.title}>Página Inicial</h1>
+      <div className={styles.filter}>
+        <h3 className={styles.search_txt}>Buscar: </h3>
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          placeholder="Filtrar por Nome"
+          value={filters.name}
+          onChange={handleFilterChange}
+        />
+        <input
+          className={styles.input}
+          type="text"
+          name="email"
+          placeholder="Filtrar por Email"
+          value={filters.email}
+          onChange={handleFilterChange}
+        />
+        <input
+          className={styles.input}
+          type="text"
+          name="lastName"
+          placeholder="Filtrar por Sobrenome"
+          value={filters.lastName}
+          onChange={handleFilterChange}
+        />
 
-      <div className={styles.buttons}>
-      <button className={styles.button} onClick={handleApplyFilters}>Aplicar Filtros</button>
-      <button className={styles.button} onClick={clearFilters}>Limpar Filtros</button>
+        <div className={styles.search_btn}>
+          <button className={styles.button} onClick={handleApplyFilters}>
+            <span className={styles.button_text}>Buscar</span>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
       </div>
     </div>
   );
